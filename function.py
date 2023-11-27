7c02bb184b5056c87a477cdfa869a4816b27e09f
brockii <89240640+brockii@users.noreply.github.com>
2023-11-28 00:34:11 +0530

    Add files via upload

--- function.py
+++ function.py
@@ -1,14 +1,12 @@
 import main
 
 
-browser_instance = None
-
 def launch_browser(browser_name="chrome", location_of_launch="https://google.co.in"):
-    global browser_instance
-    browser_instance = main.Browser(browser_name, location_of_launch)
+    main.Browser(browser_name, location_of_launch)
 
 def Wait(amount_of_time):
-    browser_instance.wait(time_duration=int(amount_of_time))
+    main.Browser.wait(time_duration=amount_of_time)
 
 def click(elem):
-    browser_instance.click_on_element(elem)
+    browser_instance = main.Browser()
+    browser_instance.click_on_element(element=elem)
--- myTest.robot
+++ myTest.robot
@@ -3,6 +3,5 @@ Library    function.py
 
 *** Test Cases ***
 Launch the browser
-    launch browser    edge    https://google.co.in
-    Wait              5
+    launch browser    chrome    https://google.co.in
     click             //*[@id="gb"]/div/div[1]/div/div[1]/a
